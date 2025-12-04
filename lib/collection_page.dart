136f6da1f3b940963febdba4ecf35ccee4c2bfba
nsamede <nsamede7@gmail.com>
2025-12-04 22:30:50 +0000

    Add top banner and header to collection page

--- lib/collection_page.dart
+++ lib/collection_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/header.dart';
+import 'package:union_shop/top_banner.dart';
 
 class CollectionPage extends StatelessWidget {
   const CollectionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("collection");
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TopBanner(),
+            Header(),
+          ],
+        ),
+      ),
+    );
   }
 }
