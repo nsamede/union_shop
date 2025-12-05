22c228d14ee9127041f85ce9064944c3bb64c79d
nsamede <nsamede7@gmail.com>
2025-12-05 00:18:03 +0000

    Convert collection page to stateful widget

--- lib/collection_page.dart
+++ lib/collection_page.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:union_shop/app_drawer.dart';
 import 'package:union_shop/app_styles.dart';
 import 'package:union_shop/header.dart';
+import 'package:union_shop/models/product.dart';
 import 'package:union_shop/top_banner.dart';
 
-class CollectionPage extends StatelessWidget {
+class CollectionPage extends StatefulWidget {
   const CollectionPage({super.key});
+
+  @override
+  State<CollectionPage> createState() => _CollectionPageState();
+}
+
+class _CollectionPageState extends State<CollectionPage> {
+  List<Product> _products = [];
+  List<Product> _displayedProducts = [];
 
   @override
   Widget build(BuildContext context) {
